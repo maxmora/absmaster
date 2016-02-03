b8b9bdb032261bdf075b6b4d459a62327eba71bf
chris <spahr.christopher@gmail.com>
2016-02-03 00:45:35 -0500

    reviewer assignment algorithm re-implementation

--- absmaster_backend/reviews.php
+++ absmaster_backend/reviews.php
@@ -18,54 +18,103 @@
 
   require_once "absmaster.php";
 
-  // excluded review list should be a hash of emails => emails that shouldn't review it
-  function assign_reviewers($user_inventory,$reviews_per_user,$excluded_review_list) {
-    $users = [];
-    foreach ($user_inventory->get_users() as $u) {
-      $users[] = $u->get_email_address();
-    }
 
-    shuffle($users); // so assignments are random
-    
-    // make hash to keep track of how many reviews each user has been assigned
+  define("REVIEWER_EXCLUSIONS", BACKEND_ROOT . '/data/excluded_reviewers.json');
+
+  function read_reviewer_exclusions() {
+    // TODO implement
+  }
+
+  function write_reviewer_exclusions() {
+    // TODO implement
+  }
+
+  function reviewer_is_allowed_for_author($author,$reviewer,$exclusions) {
+    if (isset($exclusions[$author])) {
+      if (in_array($reviewer,$exclusions[$author])) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+
+  function compile_valid_reviewers($author,$list_of_users,$excluded_reviewer_list) {
+    $non_author_users = $list_of_users;
+    // exclude the current author; you can't review yourself
+    $index = array_search($author,$non_author_users);
+    array_splice($non_author_users,$index,1);
+
+    $valid_reviewers = [];
+    foreach ($non_author_users as $u) {
+      if (reviewer_is_allowed_for_author($author,$u,$excluded_reviewer_list)) {
+        $valid_reviewers[] = $u;
+      }
+    }
+    return $valid_reviewers;
+  }
+
+  // used in assign_reviewers_alt(), not currently implemented
+  function compile_valid_reviewees($reviewer,$list_of_users,$excluded_reviewer_list) {
+    $non_reviewer_users = $list_of_users;
+    // remove this reviwer; you can't review yourself
+    $index = array_search($reviewer,$non_reviewer_users);
+    array_splice($non_reviewer_users,$index,1);
+
+    $valid_reviewees = [];
+    foreach ($non_reviewer_users as $u) {
+      if (reviewer_is_allowed_for_author($u,$reviewer,$excluded_reviewer_list)) {
+        $valid_reviewees[] = $u;
+      }
+    }
+    return $valid_reviewees;
+  }
+
+
+  // excluded review list should be a hash of emails => emails that shouldn't review it
+  function assign_reviewers($users,$reviews_per_user,$excluded_reviewer_list) {
+    // make hash table to keep track of how many reviews each user has been assigned
     $assigned_reviews = [];
     foreach ($users as $u) {
       $assigned_reviews[$u] = 0;
     }
 
-    // loop through users and assign n reviewers to each
-    // FIXME implement as an appropriate object type instead of just debug echoing!
-    foreach ($users as $u) {
-      $reviewer_pool = $users;
-      // remove just the current user, so that $reviewer_pool is everyone but that user
-      $index = array_search($u,$reviewer_pool);
-      array_splice($reviewer_pool,$index,1);
+    
+    $authors_and_their_reviewers = [];
 
-      $revs_for_user = []; // TODO implement this as right kind of object for these pairs
+    // loop through users and assign n reviewers to each
+    foreach ($users as $u) {
+      $reviewer_pool = compile_valid_reviewers($u,$users,$excluded_reviewer_list);
+      $reviewers_for_user = []; // TODO implement this as right kind of object for these pairs
+
+      shuffle($reviewer_pool);
 
       $i = $reviews_per_user;
       while ($i > 0) {
         $r = array_pop($reviewer_pool);
-        if ($assigned_reviews[$r] < $reviews_per_user && true) { // FIXME this 'true' needs to instead check that that reviewer isn't excluded for that user
-          $revs_for_user[] = $r; // TODO refactor along with correct implementation of author--reviewer pairs
-          $assigned_reviews[$r]++;
+        // has $reviewer_pool become empty?
+        if (isset($r)) {
+          if ($assigned_reviews[$r] < $reviews_per_user) {
+            $reviewers_for_user[] = $r;
+            $assigned_reviews[$r]++;
+          } else {
+            continue;
+          }
+          $i--;
         } else {
-          // TODO here, save $r to a list of people with too many, iff they aren't in the excluded list; then go back and assign reviewers from them if there is no one else available to review the abstract that has fewer reviews assigned to them
-          echo "skipped $r as rev for $u<br>";
-          continue;
+          // $reviewer_pool is empty, so we can't find a match; recurse until we don't run into this problem
+          return assign_reviewers($users,$reviews_per_user,$excluded_reviewer_list);
         }
-        $i--;
       }
 
-      // echo for debug
-      echo "$u has reviewers: ";
-      print_r($revs_for_user);
-      echo '<br>';
-
+      $authors_and_their_reviewers[$u] = $reviewers_for_user;
     }
-
-    // echo for dubug
-    echo "assigned reviews:<br>";
-    print_r($assigned_reviews);
-    // TODO end up with concatenated object of author--reviewer pairs and return that
+    
+    return $authors_and_their_reviewers;
   }
+
+  
+  function distribute_papers_to_reviewers($rev_list,$user_inv) {
+    // TODO implement
+  } 
+
--- www/absmaster/assignreviewers.php
+++ www/absmaster/assignreviewers.php
@@ -9,13 +9,68 @@
   require_once "../../absmaster_backend/reviews.php";
 
   // TODO get these from user input
-  // TODO also, this whole thing should only be operating on people who _have_ submitted reviews; check for this when having admin assign reviewers
   $reviews_per_user = 2;
-  $excluded_reviewers = ''; // TODO figure out how to implement this
+
+  // TODO implement gathering of these
+  $excluded_reviewers = []; // when implemented, this will be a hash table of reviewer => [exclusions]
+
+  // make list of users' email addresses
+  $users = [];
+  foreach ($userinventory->get_users() as $u) {
+    $users[] = $u->get_email_address();
+  }
+
+  // TODO this whole thing should only be operating on people who have submitted reviews; check for this when having admin assign reviewers?
+  $review_pairings = assign_reviewers($users,$reviews_per_user,$excluded_reviewers);
+
+
+  function generate_reviewer_assignment_table($review_list,$user_inv) {
+
+    function string_of_reviewers($user,$review_list,$user_inv) {
+      $reviewer_string = '';
+      foreach ($review_list[$user->get_email_address()] as $r) {
+        $rev_user = $user_inv->get_user_by_email_address($r);
+        $reviewer_string = $reviewer_string . $rev_user->get_first_name() . ' ' . $rev_user->get_last_name() . ' (' . $rev_user->get_email_address() . '), ';
+      }
+      return $reviewer_string;
+    }
+    $users = $user_inv->get_users();
+    $table_string = '';
+    $table_string .=
+    "<table>\n" .
+    "  <tr>\n" .
+    "    <td>" . "First name" . "</td>\n" .
+    "    <td>" . "Last name" . "</td>\n" .
+    "    <td>" . "Email address" . "</td>\n" .
+    "    <td>" . "Uploaded paper" . "</td>\n" .
+    "    <td>" . "Reviewers" . "</td>\n" .
+    "  </tr>\n";
+    foreach ($users as $u) {
+      $table_string .=
+      "  <tr>\n" .
+      "    <td>" . $u->get_first_name() . "</td>\n" .
+      "    <td>" . $u->get_last_name() . "</td>\n" .
+      "    <td>" . $u->get_email_address() . "</td>\n" .
+      "    <td>" . '"' . $u->get_uploaded_paper()['title'] . '"' . "</td>\n" .
+      "    <td>" . string_of_reviewers($u,$review_list,$user_inv) . "</td>\n" .
+      "  </tr>\n";
+    }
+    $table_string .= "</table>\n";
+    return $table_string;
+  }
+
 ?>
 
 <html>
 
-  <?php assign_reviewers($userinventory,$reviews_per_user,$excluded_reviewers); ?>
+  <h2>Assigned Reviews</h2>
+
+  <p>
+    <?php echo generate_reviewer_assignment_table($review_pairings,$userinventory); ?>
+  </p>
+
+  <p>
+    You can refresh the page to re-assign. Click the button below to distribute papers to reviewers when you are satisfied.
+  </p>
 
 </html>
