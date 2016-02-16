221460720d1695bd62e3e81f38c6bb70a5051857
chris <spahr.christopher@gmail.com>
2016-02-16 15:03:17 -0500

    push forgotten front end file

---
+++ www/absmaster/updatesettings.php
@@ -0,0 +1,41 @@
+<?php 
+  session_start();
+  require_once "../../absmaster_backend/absmaster.php";
+
+  if ($PROJECTSTATE->admin_pin_validate($_SESSION['admin_pin']) == false) {
+    die('Access denied. You must be logged in as administrator to do that.');
+  }
+
+  $report_string = '';
+
+  // only make set/write settings if they're actually changes
+  if ($_POST['signup_enablement'] == 'enable' && !$PROJECTSTATE->get_signup_enabled_status()) {
+   $PROJECTSTATE->enable_signup();
+   $PROJECTSTATE->write_project_state_data();
+   $report_string .= 'Signup is now enabled<br>';
+  } elseif ($_POST['signup_enablement'] == 'disable' && $PROJECTSTATE->get_signup_enabled_status()) {
+   $PROJECTSTATE->disable_signup();
+   $PROJECTSTATE->write_project_state_data();
+   $report_string .= 'Signup is now disabled<br>';
+  }
+
+  if ($_POST['max_users'] != $PROJECTSTATE->get_max_users()) {
+    $PROJECTSTATE->set_max_users($_POST['max_users']);
+    $PROJECTSTATE->write_project_state_data();
+    $report_string .= 'Maximum number of users has been changed to ' . $_POST['max_users'] . '.<br>';
+  }
+
+  if ($report_string == '') {
+    $report_string = 'No settings have been changed.';
+  }
+
+?>
+
+<html>
+  <p>
+    <?php echo $report_string;?>
+  </p>
+
+  <a href="admin.php">Return to admin page</a>
+
+</html>
